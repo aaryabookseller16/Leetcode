6b547f7e39514e356e06735e0ff34e0113da8092
aaryabookseller <113951802+aaryabookseller@users.noreply.github.com>
2025-12-13 14:28:53 -0600

    solved arrays-hashing/two-sum

---
+++ arrays-hashing/two-sum/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {} #store value -> idx
+
+        for i, value in enumerate(nums):
+            diff = target - value
+            if diff in map: # if we find the element and the difference
+                return [map[diff], i]
+            map[value] = i
+
+        return []
+            
