86007a7db5bd26f6d0d284687bbde7efc72ee186
aaryabookseller <113951802+aaryabookseller@users.noreply.github.com>
2026-03-03 21:51:51 -0600

    Re-solve two-sum.

--- arrays-hashing/two-sum/solution.py
+++ arrays-hashing/two-sum/solution.py
@@ -1,14 +1,22 @@
-from typing import List
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {} #store value -> idx
+        n_index = {} # keeps track of number -> index
+        answer = []
 
-        for i, value in enumerate(nums):
-            diff = target - value
-            if diff in map: # if we find the element and the difference
-                return [map[diff], i]
-            map[value] = i
+        for i in range(len(nums)): # O(n)
+            difference = target - nums[i]
+
+            # check before updating the map, in order to avoid duplicates
+            if difference in n_index: # O(1)
+                answer.append(i) # append current index
+                answer.append(n_index[difference]) # append index of corresponding element
+                return answer
+            
+            # if difference is not present, just store the n -> index pair
+            # for future use
+            n_index[nums[i]] = i
 
         return []
-            
+
+# Overall time complexity: O(n)
+# Overall space complexity: O(n)
